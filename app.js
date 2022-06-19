725cd3e958dc16a5fec6eab8dd7cb4717a7b67dc
Paul Franco <paul.franco@paulfra-ltmkbcz.internal.salesforce.com>
2022-06-18 17:18:15 -0700

    Updating features

--- app.js
+++ app.js
@@ -11,32 +11,33 @@ const generateError = (err) => {
 
 document.addEventListener("DOMContentLoaded", init);
 
+//Global variables for fetched data
+const posterLink = "https://image.tmdb.org/t/p/w500"
 let responseData; 
-
-console.log(responseData)
+let pageNum;
+let searchedMovie;
+let pageLoaded;
+let atNowPlaying;
 
 function init(count) {
 
     document.getElementById("clicker").addEventListener("click", async (ev) => {
-        
-        let pageNum = 1
+    if (document.getElementById("search").value == ""){
+        alert("Enter a movie name before searching")
+        return 
+    }
+        pageNum = 1
         searchedMovie = document.getElementById("search").value;
-        const posterLink = "https://image.tmdb.org/t/p/w500"
+        document.getElementById("search").value = ""
+
         ev.preventDefault();
         let apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=' + APIKEY + '&query='+ searchedMovie + '&page=' + pageNum;
-        console.log(apiUrl);
-        console.log(pageNum);
 
         try {
             document.getElementById("movieDisplay").innerHTML = "";
             let response = await fetch(apiUrl);
-            console.log("resp", response);
-
             responseData = await response.json();
-            console.log(responseData);
-            console.log(searchedMovie)
 
-            console.log(typeof responseData)
             if (Object.keys(responseData).length > 1){
                 for (const Index in responseData.results){
 
@@ -46,13 +47,10 @@ function init(count) {
                         Rating: responseData.results[Index].vote_average,
                         ImagePath: responseData.results[Index].poster_path,
                     }
-
-                    //console.log(movieData)
                     displayMovie(movieData, posterLink);
-
                 }
             }
-
+            pageLoaded = true
         } catch (e) {
 
             generateError(document.getElementById("search").value);
@@ -63,6 +61,106 @@ function init(count) {
 }
 
 function displayMovie(movieData, posterLink) {
+    if (movieData.ImagePath == null){
+        console.log("Image Not Found")
+    }
     document.getElementById("movieDisplay").innerHTML += `<div><p>${movieData.Title}</p> ${movieData.Rating} <img width=200px height=300px src=${posterLink + movieData.ImagePath}></div>`;
 }
 
+async function loadMore(){
+
+    if (document.getElementById("search").value == "" && pageLoaded == false){
+        alert("Enter a movie name before searching")
+        return 0
+    }
+
+    if (atNowPlaying == true){
+    pageNum += 1
+    let apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + APIKEY + '&language=en-US&page=' + pageNum;
+        try {
+            let response = await fetch(apiUrl);
+
+            responseData = await response.json();
+            if (Object.keys(responseData).length > 1){
+                for (const Index in responseData.results){
+                    const movieData = {
+                        Id: responseData.results[Index].id,
+                        Title: responseData.results[Index].original_title,
+                        Rating: responseData.results[Index].vote_average,
+                        ImagePath: responseData.results[Index].poster_path,
+                    }
+                    displayMovie(movieData, posterLink);
+                }
+            }
+
+        } catch (e) {
+            generateError(document.getElementById("search").value);
+        }
+
+    }
+
+    pageNum += 1
+    let apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=' + APIKEY + '&query='+ searchedMovie + '&page=' + pageNum;
+        try {
+            let response = await fetch(apiUrl);
+
+            responseData = await response.json();
+            if (Object.keys(responseData).length > 1){
+                for (const Index in responseData.results){
+                    const movieData = {
+                        Id: responseData.results[Index].id,
+                        Title: responseData.results[Index].original_title,
+                        Rating: responseData.results[Index].vote_average,
+                        ImagePath: responseData.results[Index].poster_path,
+                    }
+                    displayMovie(movieData, posterLink);
+                }
+            }
+
+        } catch (e) {
+            generateError(document.getElementById("search").value);
+        }
+    
+    
+}
+
+async function loadCurrentMovies(){
+
+    pageNum = 1
+    let apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + APIKEY + '&language=en-US&page=' + pageNum;
+        try {
+            
+            let response = await fetch(apiUrl);
+            responseData = await response.json();
+            if (Object.keys(responseData).length > 1){
+                for (const Index in responseData.results){
+                    const movieData = {
+                        Id: responseData.results[Index].id,
+                        Title: responseData.results[Index].original_title,
+                        Rating: responseData.results[Index].vote_average,
+                        ImagePath: responseData.results[Index].poster_path,
+                    }
+                    
+                    displayMovie(movieData, posterLink);
+                }
+                
+            }
+
+        } catch (e) {
+            generateError(document.getElementById("search").value);
+        }
+    atNowPlaying = true
+}
+
+if (movieDisplayElement == undefined){
+    loadCurrentMovies()
+}
+function returnHome(){
+    document.getElementById("movieDisplay").innerHTML = "";
+    loadCurrentMovies();
+}
+
+
+//Variable and function names
+//Image is Null
+// Styling
